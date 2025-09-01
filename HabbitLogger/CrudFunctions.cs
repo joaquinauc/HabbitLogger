3684d496ee0928f4e43f4c1b90bf231c7793720e
joaquinauc <joaquins_urrea@hotmail.com>
2025-08-31 21:52:17 -0600

    Rename project from HabbitLogger to HabitLogger

---
+++ HabbitLogger/CrudFunctions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace HabbitLogger;
+
+internal class CrudFunctions
+{
+    internal static void Insert(SqliteCommand command)
+    {
+
+    }
+
+    internal static void Delete(SqliteCommand command)
+    {
+
+    }
+}
